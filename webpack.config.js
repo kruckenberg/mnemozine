5729ef0fb216e18c13c75647753a18c17e2c6e6e
Nick Kruckenberg <nkruckenberg@gmail.com>
2020-08-11 13:01:06 -0400

    restore index.html to root

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
 	mode: 'development',
-	entry: 'index.js',
+	entry: './client/index.js',
 	output: {
 		path: path.resolve(__dirname, 'build'),
 		filename: 'bundle.js',
